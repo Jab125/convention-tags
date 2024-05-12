53c98bf493d00eb98044f2c1e5e42d6c8ceb2222
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-05-12 12:24:23 +1000

    24w19b

--- java/dev/jab125/convention/tags/TagGeneration.java
+++ java/dev/jab125/convention/tags/TagGeneration.java
@@ -26,8 +26,8 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class TagGeneration {
-	public static final boolean legacyNames = true; // Set to false in 24w19a+
-	public static final String FABRIC_CONVENTION_TAGS_VERSION = "2.1.0+619abec268";
+	public static final boolean legacyNames = false; // Set to false in 24w19a+
+	public static final String FABRIC_CONVENTION_TAGS_VERSION = "2.1.1+5e47b9cb4f";
 	public static final String NEOFORGE_VERSION = null;
 	public static final Path GENERATION = Path.of("generation");
 	public static final List<Tag.TagBuilder> mutableTags = new ArrayList<>();
